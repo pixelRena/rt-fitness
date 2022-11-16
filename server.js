f2d8eb8e51b99154a7fc853b776cc6769d1001b7
Serenuy <dserena01@yahoo.com>
2022-11-15 23:01:40 -0600

    Update server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const aws = require("aws-sdk");
+const cors = require("cors");
 dotenv.config()
 
 const app = express()
@@ -12,7 +13,7 @@ const buildPath = path.join(__dirname, 'build')
 
 app.use(express.static(buildPath))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(cors())
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'))
